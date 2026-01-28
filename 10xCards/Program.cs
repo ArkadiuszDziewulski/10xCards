db221fcbc00505887a711835cabf484f076e73b5
Arkadiusz Dziewulski <arkadiuszd@tiger-soft.pl>
2026-01-28 11:52:45 +0100

    Update Program file

--- 10xCards/Program.cs
+++ 10xCards/Program.cs
@@ -1,11 +1,17 @@
 using _10xCards;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Supabase;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var url = builder.Configuration["Supabase:Url"] ?? "";
+var key = builder.Configuration["Supabase:Key"] ?? "";
+builder.Services.AddScoped(_ => new Supabase.Client(url, key, new SupabaseOptions { AutoConnectRealtime = true }));
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
